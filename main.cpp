74f270e48b46f2bdb96d5b968059448d4100d773
JRWaugh <joshua@waugh.zone>
2020-09-02 10:03:09 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "GCodeParser.h"
+#include "PlotterDebug.h"
+
+int main() {
+    GCodeParser parser;
+    PlotterDebug plotter(&parser, [](auto buffer) {
+        std::cout << buffer;
+        });
+    
+    std::ifstream infile("log01.txt");
+    std::string buffer;
+
+    if (infile.is_open())
+        while (std::getline(infile, buffer))
+            parser.parse(buffer.c_str());
+}
