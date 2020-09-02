5994f63f4d99c54e050e9d93401af67daaa981f6
JRWaugh <joshua@waugh.zone>
2020-09-02 14:19:30 +0300

    Update lpc_main.cpp

--- lpc_main.cpp
+++ lpc_main.cpp
@@ -8,30 +8,30 @@
 #include "PlotterDebug.h"
 
 int main(void) {
-	SystemCoreClockUpdate();
-	Board_Init();
-	heap_monitor_setup();
+    SystemCoreClockUpdate();
+    Board_Init();
+    heap_monitor_setup();
 
-	xTaskCreate([](auto) {
-		PlotterDebug plotter([](auto buffer) { Board_UARTPutSTR(buffer); });
-		GCodeParser parser(&plotter);
-		std::array<char, 64> buffer;
-		auto count = buffer.begin();
+    xTaskCreate([](auto) {
+        PlotterDebug plotter([](auto buffer) { Board_UARTPutSTR(buffer); });
+        GCodeParser parser(&plotter);
+        std::array<char, 64> buffer;
+        auto count = buffer.begin();
 
-		while (true) {
-			if (int in = Board_UARTGetChar(); in != EOF) {
-				*count++ = in;
+        while (true) {
+            if (int in = Board_UARTGetChar(); in != EOF) {
+                *count++ = in;
 
-				if (in == '\n' || in =='\r' || count == buffer.end()) {
-					*--count = '\0';
-					parser.parse(buffer.data());
-					count = buffer.begin();
-				}
-			}
-		}
-	}, "vTaskUart", configMINIMAL_STACK_SIZE + 128, nullptr, tskIDLE_PRIORITY + 1UL, nullptr);
+                if (in == '\n' || in =='\r' || count == buffer.end()) {
+                    *--count = '\0';
+                    parser.parse(buffer.data());
+                    count = buffer.begin();
+                }
+            }
+        }
+    }, "vTaskUart", configMINIMAL_STACK_SIZE + 128, nullptr, tskIDLE_PRIORITY + 1UL, nullptr);
 
-	vTaskStartScheduler();
+    vTaskStartScheduler();
 
-	return 1;
+    return 1;
 }
