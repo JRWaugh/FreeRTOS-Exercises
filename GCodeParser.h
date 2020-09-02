29df4dfd45de1ff9b4f372c339bd9daf429bc4b4
JRWaugh <joshua@waugh.zone>
2020-09-02 13:43:13 +0300

    Update GCodeParser.h

--- GCodeParser.h
+++ GCodeParser.h
@@ -1,19 +1,22 @@
-#pragma once
-#include <vector>
-#include <algorithm>
+#ifndef GCODEPARSER_H_
+#define GCODEPARSER_H_
+
 #include "PlotterInterface.h"
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
 
 class GCodeParser {
 public:
+	GCodeParser(PlotterInterface* plotter = nullptr);
     void parse(char const* g_code);
-    void attach(PlotterInterface* plotter) noexcept;
-    void detach(PlotterInterface* plotter) noexcept;
 
 private:
-    std::vector<PlotterInterface*> plotters;
+    PlotterInterface* plotter;
 
     constexpr static auto kMalformedCode = "Malformed code\r\n";
     constexpr static auto kUnknownCode = "Unknown code\r\n";
     constexpr static auto kNotAGCode = "Not a GCode\r\n";
 };
 
+#endif /* GCODEPARSER_H_ */
